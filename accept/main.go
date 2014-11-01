62d93a4ba2040ead774e0484a144c22523c06ea1
Colin Humphreys <colin@hatofmonkeys.com>
2014-11-01 21:24:51 +0000

    Golang app writes to redis

--- accept/main.go
+++ accept/main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
 	"os"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/mitchellh/mapstructure"
+	"gopkg.in/redis.v2"
 )
+
+func redisClient() *redis.Client {
+	return redis.NewTCPClient(&redis.Options{
+		Addr:     getRedisAddr(),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
+
+func getRedisAddr() string {
+	redis, err := getServices().WithName("redis")
+	if err != nil {
+		panic(err)
+	}
+	return redis.Credentials["hostname"] + ":" + redis.Credentials["port"]
+}
+
+func getServices() *cfenv.Services {
+	var rawServices map[string]interface{}
+	servicesVar := os.Getenv("VCAP_SERVICES")
+	if err := json.Unmarshal([]byte(servicesVar), &rawServices); err != nil {
+		return nil
+	}
+
+	services := make(cfenv.Services)
+	for k, v := range rawServices {
+		var serviceInstances []cfenv.Service
+		if err := mapstructure.Decode(v, &serviceInstances); err != nil {
+			return nil
+		}
+		services[k] = serviceInstances
+	}
+	return &services
+}
 
 type Word struct {
 	Word string
@@ -17,6 +56,9 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
 	word := &Word{Word: r.FormValue("word")}
+	if err := redisClient().RPush("words", word.Word).Err(); err != nil {
+		panic(err)
+	}
 	t, _ := template.ParseFiles("thanks.html")
 	t.Execute(w, word)
 }
@@ -25,4 +67,5 @@ func main() {
 	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/add", addHandler)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
 }
